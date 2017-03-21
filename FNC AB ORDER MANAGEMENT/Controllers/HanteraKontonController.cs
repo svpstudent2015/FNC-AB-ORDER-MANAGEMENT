4fbffe751f5690cfe1647985eebd97d93e04a822
Kevin <Kevin@DESKTOP-9AUBG4B>
2017-03-21 15:48:30 +0100

    Lagt in Inmätningat/Utmätningat/Hantera Konton Views samt Controllers

---
+++ FNC AB ORDER MANAGEMENT/Controllers/HanteraKontonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FNC_AB_ORDER_MANAGEMENT.Controllers
+{
+    public class HanteraKontonController : Controller
+    {
+        // GET: HanteraKonton
+        public ActionResult HanteraKonton()
+        {
+            return View();
+        }
+    }
+}
