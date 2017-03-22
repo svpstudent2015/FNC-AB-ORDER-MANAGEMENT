a5536171b615975e5a1def084f52c3bc0e287e50
Kevin <Kevin@DESKTOP-9AUBG4B>
2017-03-22 15:29:37 +0100

    Skapat InmätningarModel samt nyInmätning funktion för att spara ner i databasen

---
+++ DAL/RepositoryFolder/InmatningarRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace DAL.RepositoryFolder
+{
+    public class InmatningarRepository : Repository<INMATNINGAR>
+    {
+
+    }
+}
---
+++ DAL/RepositoryFolder/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.RepositoryFolder
+{
+    public class Repository<T> where T : class 
+    {
+        public void Add(T item)
+        {
+            try
+            {
+                using (var db = new FNCOrderHanteringEntitiesConnections())
+                {
+                    db.Set<T>().Add(item);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
